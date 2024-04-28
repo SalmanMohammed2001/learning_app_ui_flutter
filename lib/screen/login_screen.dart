2f125ee2c4aee0625c43287eff4b1ea899909a24
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-28 23:44:22 +0530

    separate widget social implementation

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -19,6 +21,72 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
       backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: [
+            const Text(
+              "Learning App",
+              style: TextStyle(
+                  color: Colors.purple,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Enter your login Details to access your account",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.purple, fontSize: 18, height: 0.9),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            SocialButton(size: size)
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class SocialButton extends StatelessWidget {
+  const SocialButton({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: size.width * 0.4,
+          height: 50,
+          decoration: BoxDecoration(
+              color: Colors.blue, borderRadius: BorderRadius.circular(15)),
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                FontAwesomeIcons.facebook,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                "Facebook",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
