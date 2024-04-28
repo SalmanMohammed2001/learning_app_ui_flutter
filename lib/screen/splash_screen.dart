84067bf75f6b89e54100da0c50956f8b4a054575
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-04-28 22:42:27 +0530

    splash screen and welcomePage implementation

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,53 @@
+
+
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:learning_app_ui/screen/welcome_screen.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    Timer(const Duration(seconds:3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+        return const WelcomePage();
+      },));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.purple,
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.cast_for_education,
+            color: Colors.white,
+            size: 60,
+          ),
+          Text(
+            "Learning App",
+            style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                fontStyle: FontStyle.italic),
+          ),
+      CupertinoActivityIndicator(color: Colors.white,)
+        ],
+      )),
+    );
+  }
+}
